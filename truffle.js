3a084d14f8fa2b7db82dfab8425d9805ac2f04b1
Aliaksei Hiatsevich <aliaksei.hiatsevich@gmail.com>
2018-06-18 22:37:36 +0300

    Add rinkeby config. Reuse already deployed in rinkeby VUToken

--- truffle.js
+++ truffle.js
@@ -1,17 +1,26 @@
-module.exports = {
-networks: {
-    development: {
-      host: 'localhost',
-      port: 8545,
-      network_id: '*', // Match any network id
-      gas: 5700000
-    }
-  },
-  solc: {
-    optimizer: {
-      enabled: true,
-      runs: 200
-    }
-  },
-  migrations_directory: './migrations'
+var HDWalletProvider = require("truffle-hdwallet-provider");
+function getMnemonic(){
+    return require('fs').readFileSync("./secrets.json", "utf8").trim();
 }
+
+module.exports = {
+    networks: {
+        main: {
+            network_id: 1,
+            provider: new HDWalletProvider(getMnemonic(), "password", 'https://mainnet.infura.io/'),
+            gas: 4700000,
+            gasPrice: 1000000000
+        },
+        rinkeby:{
+            network_id:4,
+            provider: new HDWalletProvider(getMnemonic(), "password", 'https://rinkeby.infura.io/'),
+            gas: 4700000,
+            gasPrice: 10000000000
+        },
+        development: {
+            host: "127.0.0.1",
+            port: 8545,
+            network_id: "*" // Match any network id
+        }
+    }
+};
